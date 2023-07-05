d467c307acbd4ce220853c9b0fba4079a164ba3b
rsouvik <sales@getmypickly.com>
2023-07-05 09:31:10 -0700

    new changes

--- my-nft/contracts/MyNFT.sol
+++ my-nft/contracts/MyNFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-//Contract
+//Contract here
 contract MyNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
