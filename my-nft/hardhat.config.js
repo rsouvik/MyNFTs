326df75d1b537b54152ecfbf2fc7b7532c2496eb
rsouvik <sales@getmypickly.com>
2023-07-12 12:57:25 -0700

    new changes

--- my-nft/hardhat.config.js
+++ my-nft/hardhat.config.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
-   solidity: "0.8.1",
-   defaultNetwork: "ropsten",
+   solidity: "0.8.9",
+   defaultNetwork: "goerli",
    networks: {
       hardhat: {},
       ropsten: {
