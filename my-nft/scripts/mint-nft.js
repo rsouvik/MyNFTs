cabc65daffa1289cdbf4b0ed71a905fabf68fae0
rsouvik <sales@getmypickly.com>
2023-07-14 12:19:01 -0700

    new changes

--- my-nft/scripts/mint-nft.js
+++ my-nft/scripts/mint-nft.js
@@ -1,10 +1,54 @@
 require("dotenv").config()
 const API_URL = process.env.API_URL
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
 
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json")
+const contractAddress = "0x5C2bC5065a1C3a167777bf221E072249be83CcfA"
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
 
-console.log(JSON.stringify(contract.abi))
+async function mintNFT(tokenURI) {
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest") //get latest nonce
+
+    //the transaction
+    const tx = {
+        from: PUBLIC_KEY,
+        to: contractAddress,
+        nonce: nonce,
+        gas: 500000,
+        data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+    }
+
+    const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
+    signPromise
+        .then((signedTx) => {
+            web3.eth.sendSignedTransaction(
+                signedTx.rawTransaction,
+                function (err, hash) {
+                    if (!err) {
+                        console.log(
+                            "The hash of your transaction is: ",
+                            hash,
+                            "\nCheck Alchemy's Mempool to view the status of your transaction!"
+                        )
+                    } else {
+                        console.log(
+                            "Something went wrong when submitting your transaction:",
+                            err
+                        )
+                    }
+                }
+            )
+        })
+        .catch((err) => {
+            console.log("Promise failed:", err)
+        })
+}
+
+mintNFT("ipfs://QmYYchZQJ5fwRqRH8rmB7e27N8Y7xsnuKr9jwwCW3W5dRX")
+
 
 
